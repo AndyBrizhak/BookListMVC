0e3df4fed5bab7c9f20dcab90893d09ee0e3eb02
Андрей Брижак <andrewbrizhak@gmail.com>
2020-05-26 18:03:21 +0800

    add Db context

--- BookListMVC/Models/ApplicationDbContext.cs
+++ BookListMVC/Models/ApplicationDbContext.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookListMVC.Models
 {
-    public class ApplicationDbContext
-    {
-    }
+    
+        public class ApplicationDbContext : DbContext
+        {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+            {
+
+            }
+
+            public DbSet<Book> Books { get; set; }
+        }
+    
 }
